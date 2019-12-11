1478483308c2e6ca644979b9caf1cbfde426796c
Arman199116 <arman.ochinyan@gmail.com>
2019-12-11 16:42:06 +0400

    have changed the variable statements from 'var' to 'const' and 'let' and and deleted try/catch in the exec_cmd.js file

--- web/exec_cmd.js
+++ web/exec_cmd.js
@@ -1,19 +1,15 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-var fs = require('fs');
+const fs = require('fs');
 
-var bteq_path = `./bteq_dir/psql -m 1 < ${__dirname}/`;
-var out_file = ` > ${__dirname}/out.bteq`;
+const bteq_path = `./bteq_dir/psql -m 1 < ${__dirname}/`;
+const out_file = ` > ${__dirname}/out.bteq`;
 
 module.exports = async(cmd_file) => {
     try {
         const {stdout, stderr} = await exec(`${bteq_path}${cmd_file}${out_file}`);
         console.log('stdout:', stdout);
-        try {
-            fs.unlinkSync(cmd_file);
-        } catch(err) {
-            console.error(err);
-        }
+        fs.unlinkSync(cmd_file);
     } catch(err) {
         console.error(err);
     };
--- web/server.js
+++ web/server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyparser = require('body-parser');
-var fs = require('fs');
-var open = require('open');
-var exec_cmd = require('./exec_cmd');
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const fs = require('fs');
+const open = require('open');
+const exec_cmd = require('./exec_cmd');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyparser.urlencoded({extended : false}));
 
-var cmd_file = 'cmd.txt';
+const cmd_file = 'cmd.txt';
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -17,35 +17,43 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    var {'logon' : log, 'set-width' : wid, 'select' : sel} = req.body;
-    var logon = undefined;
-    var set = undefined;
-    var select = undefined;
+    const {'logon' : log, 'set-width' : wid, 'select' : sel} = req.body;
+    let logon = "";
+    let set = "";
+    let select = "";
 
-    if (!!log.trim()) {
-        logon = log.match(/^\.[a-zA-Z0-9_$]/g);
+    if (!!log && !!log.trim()) {
+        logon = log.match(/^\.[a-zA-Z0-9_$]+/g);
     }
-    if (!!wid.trim()) {
-        set = wid.match(/^\.[a-zA-Z0-9_$]/g);
+    if (!!wid && !!wid.trim()) {
+        set = wid.match(/^\.[a-zA-Z0-9_$]+/g);
     }
-    if (!!sel.trim()) {
+    if (!!sel && !!sel.trim()) {
         select = sel.match(/^select/gi);
     }
     if (logon === null || set === null || select === null) {
         res.render('bteq', {data : "Incorrect input value"});
     } else {
-        fs.writeFile(cmd_file, `${log}\n${wid}\n${sel}`, async function(err) {
-            if (err) throw err;
-            await exec_cmd(cmd_file);
-            fs.readFile('out.bteq', 'utf8', function(err, data) {
+        try {
+            fs.writeFile(cmd_file, `${log}\n${wid}\n${sel}`, async(err) => {
                 if (err) throw err;
-                res.render('bteq', {data});
+                await exec_cmd(cmd_file);
+                try {
+                    fs.readFile('out.bteq', 'utf8', (err, data) => {
+                        if (err) throw err;
+                        res.render('bteq', {data});
+                    });
+                } catch(err) {
+                    if (err) console.error(err);
+                }
             });
-        });
+        } catch(err) {
+            if (err) console.error(err);
+        }
     }
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log("BTEQ app listening at http://127.0.0.1:%s", port);
     open(`http://127.0.0.1:${port}`);
 });
